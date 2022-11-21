7be18dc34affca2aba51c6a6bb99ef64f44613ed
Katie A <katie.t.abraham@gmail.com>
2022-11-21 09:24:26 -0600

    router working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import './App.css';
-import Header from './components/Header.js';
+import React, { Component} from 'react';
+import { MemoryRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { Icon } from '@iconify/react'
+import ThemeButton from './components/ThemeButton.js';
 import Latest from './components/Latest.js';
-import Footer from './components/Footer.js';
+import Starred from './components/Starred.js';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <Header />
-      <Latest />
-      <Footer />
+      <Router>
+        <div className='header'>
+          <div className='header-left'>
+                <Icon icon="fa6-brands:y-combinator" color="#f56f36" />
+                <h1>Hacker News</h1>
+                <Link to="/latest">latest</Link>
+                <span> | </span>
+                <Link to="/starred">starred</Link>
+            </div>
+            <div className='header-right'>
+                <ThemeButton />
+            </div>
+        </div>
+        <div className='content'>
+          <Routes>
+            <Route path="/latest" element={<Latest />} />
+            <Route path='/starred' element={<Starred />} />
+          </Routes>
+        </div>
+        <div className='footer'>
+          <p>Hacker News</p>
+            <div>
+                <Link to="/latest">latest</Link>
+                <span> | </span>
+                <Link to="/starred">starred</Link>
+            </div>
+        </div>
+        </Router>
     </div>
   );
+  }
 };
 
 export default App;
